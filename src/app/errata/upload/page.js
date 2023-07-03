677264d8b585b9db788d1614012fae500a0b0fb6
TeddyPickerAC <hangyul010101@gmail.com>
2023-07-03 20:17:23 +0900

    errata-upload-debug

--- src/app/errata/upload/page.js
+++ src/app/errata/upload/page.js
@@ -34,6 +34,29 @@ const UPLOAD_QUERY = gql`
   }
 `;
 
+const QUERY_LIST = gql`
+  query MyQuery {
+    errataList {
+      id
+      createdAt
+      description
+      file
+      image
+      link
+      title
+      updatedAt
+    }
+  }
+`;
+
+const DELETE_ERRATA = gql`
+  mutation Delete($id: String!) {
+    removeErrata(errataId: $id) {
+      __typename
+    }
+  }
+`;
+
 const SignupStyle = {
   wrapper: "flex flex-col w-full py-3",
   label: "text-xl font-semibold mb-2",
@@ -270,6 +293,62 @@ function GetCorrectionList() {
       <div className="border border-slate-300 rounded-xl p-10">
         <CorrectionMiniContent />
       </div>
+    </div>
+  );
+}
+
+function CorrectionMiniContent() {
+  const { loading, error, data } = useQuery(QUERY_LIST);
+  const [mutation, set] = useMutation(DELETE_ERRATA);
+
+  return (
+    <div>
+      {loading ? (
+        <h1 className="w-full flex flex-col justify-center items-center font-extrabold text-2xl my-4">
+          로딩중..
+        </h1>
+      ) : !data ? (
+        <h1 className="w-full flex flex-col justify-center items-center font-extrabold text-2xl my-4">
+          서버로부터 데이터를 가져오지 못했습니다.
+        </h1>
+      ) : data.errataList.length === 0 ? (
+        <h1 className="w-full flex flex-col justify-center items-center font-extrabold text-2xl my-4">
+          정오표가 없습니다.
+        </h1>
+      ) : (
+        data.errataList.map(({ title, updatedAt, id }, i) => {
+          const parsedTime = updatedAt.split("T");
+
+          return (
+            <div
+              className="py-8 border-b hover:shadow-lg hover:transition-shadow "
+              key={i}
+            >
+              <div className="geist-wrapper flex md:flex-row flex-col md:gap-0 gap-5 md:justify-between justify-center items-center">
+                <div className="flex flex-col justify-center gap-3">
+                  <div className="md:text-2xl text-lg font-extrabold md:py-4 py-2">
+                    <h1>{title}</h1>
+                  </div>
+                  <div className="md:text-xl text-base">
+                    최근 변경일 : {parsedTime[0]}
+                  </div>
+                  <button
+                    onClick={
+                      (useCallback(async () => {
+                        await mutation({ variables: { id: id } });
+                      }),
+                      [])
+                    }
+                    className="p-3 text-lg text-red font-bold"
+                  >
+                    삭제하기
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
